5972279472476159b0fc35312100f99956a25286
James Stephenson <2573758S@student.gla.ac.uk>
2022-01-25 10:11:59 +0000

    test for PyCharm

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,9 @@ def index(request):
     # Note the key boldmessage matches to {{ boldmessage }} in the template! 
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     
-    # Return a rendered response to send to the client. # We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use.
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
